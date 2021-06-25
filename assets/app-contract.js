d100b952b3c4e6098ad43ba94040d32ba128f994
shimmyshine <jefflhuntjr@aol.com>
2021-06-24 23:28:06 -0500

    Add Mooncoin ABI

--- assets/app-contract.js
+++ assets/app-contract.js
@@ -314,7 +314,7 @@ $(function () {
     $("#preloaded_contracts").change(function() {
         var selectedName = $(this).val();
         
-        var arrayAssoc = { "hrc20_standard": hrc20_standard_abi, "tj_mintable": tj_mintable_abi, "tj_hardcap": tj_hardcap_abi, "ript_io_token_creator": ript_io_token_creator_abi, "onchainmining": onchainmining_abi };
+        var arrayAssoc = { "hrc20_standard": hrc20_standard_abi, "tj_mintable": tj_mintable_abi, "tj_hardcap": tj_hardcap_abi, "ript_io_token_creator": ript_io_token_creator_abi, "onchainmining": onchainmining_abi, "tj_mooncoin": mooncoin_abi };
 
         //console.log(arrayAssoc[selectedName]);
 
